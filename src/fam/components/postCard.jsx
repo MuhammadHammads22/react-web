1eb3ac89854adb5569aa5ebcb6088269a7e1e732
Abu-Ubaida <abuubaida901@gmail.com>
2024-02-19 23:20:54 +0500

    updated

--- src/fam/components/postCard.jsx
+++ src/fam/components/postCard.jsx
@@ -2,21 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "../assets/css/PostCard.css"
 import { multiFormatDateString } from '../../lib/utils/DateConvertor'
+import VideoPlayer from './VideoPlayer'
 
 const PostCard = ({ post }) => {
   return (
     <div className="post-card">
       <div className='flex-between '>
         <div className='flex items-center gap-3'>
-          <Link to={`/profile/${post.creator.$id}`} className='rounded-xl'>
-            <video 
-            src= {post.seeker_vid || 'src/assets/icons/profile-placeholder.svg'}
-            alt="creator"
-            className='rounded-full w-12 lg:h-12' 
-            width={500}
-            height={200}
-            />
-          </Link>
+          
+          <VideoPlayer src={post.seeker_vid} />
 
           <div className="flex flex-col ">
             <p className='base-medium lg:body-bold text-light-1'>
