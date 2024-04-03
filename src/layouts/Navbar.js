624fd7e442c07325fdbc7605c440a1fe4aac07d0
Abu-Ubaida <abuubaida901@gmail.com>
2024-04-04 00:05:47 +0500

    Updated AddaZakat

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -10,21 +10,14 @@ const Navbar = () => {
     <Box sx={{ flexGrow: 1 }}>
     <AppBar position="fixed" color="info" style={{ backgroundColor: '#2d3740'}}>
         <Toolbar>
-          <Typography variant='h5' component="div" sx={{ flexGrow: 1 }}>AddaZakat</Typography>
+          <Typography variant='h5' component="div" sx={{ flexGrow: 1 }}>AdaZakat</Typography>
 
-          <Button
-              component={NavLink}
-              to="/createpost"
-              sx={{
-                color: 'white',
-                textTransform: 'none',
-                '&:hover': {
-                  backgroundColor: '#5c1566', // Change color on hover if needed
-                },
-              }}
-            >
+          <Button component={NavLink} to='/post' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }}>Post</Button>
+
+          <Button component={NavLink} to="/createpost" sx={{color: 'white', textTransform: 'none', '&:hover': { backgroundColor: '#5c1566',},}}>
             <GrAdd /> Create Post
           </Button>
+
 
           <Button component={NavLink} to='/profiles' style={({ isActive }) => { return { backgroundColor: isActive ? '#6d1b7b' : '' } }} sx={{ color: 'white', textTransform: 'none' }} >Profiles</Button>
 
