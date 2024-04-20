370cfaf86529fbd33e37681267d68c3b0a83800d
Muhammad Moinuddin <moinuddin010203@gmail.com>
2024-04-21 01:19:32 +0500

    Update UI of comments (tried to implement like youtube)

--- src/post/components/comments.js
+++ src/post/components/comments.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { AiOutlineLike } from "react-icons/ai";
+import { BiDislike } from "react-icons/bi";
 import { multiFormatDateString } from '../../lib/utils/DateConvertor';
 import '../assets/css/comments.css';
 
-const CommentList = ({ comment }) => {
+const Comment = ({ comment, onUpvote, onDownvote, onReply }) => {
   return (
-    <div className='flex comment-object'>
-      <p className='ml-4 text-gray-600'>{comment.user}</p>
-      <p className='ml-4 text-gray-600'>{comment.body}</p>
-      <p className='ml-4 text-gray-600'>{multiFormatDateString(comment.created)}</p>     
+    <div className='mt-4'>
+      <div className='flex justify-between'>
+        <span className='text-lg font-bold'>{comment.user}</span>
+        <span className='mr-30 text-gray-600'>{multiFormatDateString(comment.created)}</span>     
+        {/* <span className='text-gray-600'>{comment.username}</span> */}
+      </div>
+      <p className='text-gray-600'>{comment.body}</p>
+      <div className='flex items-center'>
+        <button onClick={() => onUpvote(comment.id)} className='text-gray-600 mr-4'><AiOutlineLike /></button>
+        <button onClick={() => onDownvote(comment.id)} className='text-gray-600 mr-4'><BiDislike /></button>
+        <button onClick={() => onReply(comment.id)} className='text-gray-600'>Reply</button>
+      </div>
     </div>
   )
 }
 
-export default CommentList;
+export default Comment;
